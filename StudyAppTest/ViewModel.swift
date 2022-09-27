e224bbc3ae6052492901fd7bc816aac60187f038
mullar4 <ramesh.mullaputi@medtronic.com>
2022-09-27 16:15:05 +0530

    comited unit tests

---
+++ StudyAppTest/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  StudyAppTest
+//
+//  Created by Mullaputi Ramesh on 27/09/22.
+//
+
+import Foundation
+
+struct ViewModel {
+    func getEmail() -> String {
+        return "a@a.com"
+    }
+}
---
+++ StudyAppTestTests/ViewModelTests.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModelTests.swift
+//  StudyAppTestTests
+//
+//  Created by Mullaputi Ramesh on 27/09/22.
+//
+
+import XCTest
+@testable import StudyAppTest
+
+class ViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+       let vm = ViewModel()
+        XCTAssertTrue(vm.getEmail() == "a@a.com")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
