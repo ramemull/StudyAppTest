a069a0a190ae43488bbe6a93e3038b1eb06a6d46
mullar4 <ramesh.mullaputi@medtronic.com>
2022-09-27 16:15:33 +0530

    fixed error

--- StudyAppTest/ViewController.swift
+++ StudyAppTest/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test)
+        print("test")
         // Do any additional setup after loading the view.
     }
 
